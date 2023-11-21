1c67dd86bf830dc2ff3c8f91f6dbc0cd809d522a
Light <47911535+LightAPIs@users.noreply.github.com>
2023-11-21 21:34:24 +0800

    fix: Fix invalid copying in exported HTML file
    resolve: #3

--- src/postMessage.js
+++ src/postMessage.js
@@ -1,16 +1,19 @@
 window.copyCodeBlocksPostMessage = function (ele, pluginId) {
-  webviewApi
-    .postMessage(
-      pluginId,
-      document
-        .querySelector('#' + ele.id)
-        .closest('.joplin-editable')
-        .querySelector('.joplin-source').textContent
-    )
-    .then(response => {
+  const text = document
+    .querySelector('#' + ele.id)
+    .closest('.joplin-editable')
+    .querySelector('.joplin-source').textContent;
+  try {
+    webviewApi.postMessage(pluginId, text).then(response => {
       console.info('Got response in content script: ' + response);
-      ele.title = 'Copied';
-      ele.classList.add('copied-code-blocks');
     });
+  } catch (err) {
+    console.error(err);
+    navigator.clipboard.writeText(text);
+  }
+
+  ele.title = 'Copied';
+  ele.classList.add('copied-code-blocks');
+
   return false;
 };
